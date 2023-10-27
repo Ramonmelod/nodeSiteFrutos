ad8b5d820009f887a83ea22745df63cd140ae76a
ramonmelod <ramonmelo.com@gmail.com>
2023-10-27 12:05:41 -0300

    código rotas alterado para gerar o html mostrando logo

--- rotas/rotas.js
+++ rotas/rotas.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const rotas = express.Router()
+const path = require('path') // possibilita o uso de path.join para localização do html
 
-rotas.get('/',(req, res)=>{
-    res.send('frutos feito a mão principal')
+rotas.use(express.static(path.join(__dirname, '..', 'pagina')))  //  o middleware express.static serve arquivos estáticos da pasta "pagina"
 
+rotas.get('/',(req, res)=>{  
+    const filePath = path.join(__dirname, '..' ,'pagina', 'index.html') // o '..' faz com que path.join procure a pasta pagina na pasta pai de rotas.js
+    res.sendFile(path.join(filePath))
 })
 
 rotas.get('/nossahistoria',(req, res)=>{
