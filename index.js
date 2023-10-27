169144221ee287102918b2ffa97ecc61d9567209
ramonmelod <ramonmelo.com@gmail.com>
2023-10-27 11:11:13 -0300

    versão conseguindo rodar o html do site ( ainda não apresenta o logo.png corretamente)

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const rotas =  require('./rotas/rotas')
 const porta = process.env.PORT || 8080
 const app = express()
+const path = require('path')
 
 app.use('/',rotas)
 
 app.use('*',(req, res)=>{  // '*'atua como um middleware de manipulação de requisições curinga (catch-all)
-
-    res.send('Portal frutos feito a mão')
+    const filePath = path.join(__dirname, 'pagina', 'index.html')
+    res.sendFile(path.join(filePath))
 })
 
 app.listen(porta,console.log('servidor rodando'))
